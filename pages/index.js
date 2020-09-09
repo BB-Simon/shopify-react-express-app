bc4805b806f5caca33036c1213db3911b59d0e20
BB-Simon <simon.chowdery@gmail.com>
2020-09-08 17:25:52 -0800

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { EmptyState, Layout, Page } from "@shopify/polaris";
-const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
+// const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";4
+const img =
+  "https://polaris.shopify.com/assets/f6eda9771602775955152895b69914e4.png";
 
 const Index = () => {
   return (
